f004140cc99c896dd8357b85f003d30b2fceaa6b
Maydana Exequiel <maydanarkt@gmail.com>
2022-09-15 19:42:52 -0300

    modifique variables de entorno

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,17 +1,18 @@
 const { Sequelize } = require("sequelize");
 
 require('dotenv').config()
-const password = process.env.PASSWORD_POSTGRESLQ
-const dataB = process.env.DATABASE
-const portt = process.env.PORT_POSTGRE
+const dbUser = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const dataB = process.env.DB
+const localHost = process.env.DB_HOST
 
 const db = new Sequelize({
   dialect: "postgres",
-  host: "localhost",
-  username: 'postgres',
+  host: localHost,
+  username: dbUser,
   password: password,
   database: dataB,
-  port: portt,
+  port: 5432,
 });
 
 module.exports = {
